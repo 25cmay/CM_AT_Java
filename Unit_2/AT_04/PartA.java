b9eda73d7d3b9ac37562e2226f43a47e97a5f437
25cmay <146380979+25cmay@users.noreply.github.com>
2023-10-08 22:36:18 -0400

    AT_04
    Fixed AT_04 PartA

--- Unit_2/AT_04/PartA.java
+++ Unit_2/AT_04/PartA.java
@@ -10,15 +10,15 @@ public class PartA {
         Scanner in = new Scanner(System.in);
         // Get Variables
         double dollars;
-        int cents;
+        double cents;
         System.out.print("Enter your amount of money in dollar cent format (XX.xx): ");
         double money = in.nextDouble();
         
         // Calculate
-        dollars = money % 100;
-        money = cents;
+        dollars = (int)money;
+        money = money * 100;
+        cents = money - (dollars*100);
 
-        System.out.print("You have " + dollars + "dollars and " + cents + "cents.");
-
+        System.out.print("You have " + (int)dollars + " dollars and " + (int)cents + " cents");
     }
 }
