7c4faecad84ddc4cb93fdcd6994f63ea9c4687c9
25cmay <146380979+25cmay@users.noreply.github.com>
2023-10-11 23:30:38 -0400

    Unit_02_Project
    Unit_02_Project finished

---
+++ Unit_2/Unit_02_Project/Project.java
@@ -0,0 +1,34 @@
+/*
+ * Caleb May
+ * Mr. Eng
+ * AT Java
+ */
+import java.util.Scanner;
+
+public class Project {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        // Set Variables
+        
+        System.out.print("Enter the time in seconds: ");
+        int time = in.nextInt();
+
+        final int SECINHOUR = 3600;
+        final int SECINMINUTE = 60;
+        final int SECINSECOND = 1;
+
+        int hours;
+        int minutes;
+        int seconds;
+
+        // Calculate
+        hours = time / SECINHOUR;
+        time = time - (SECINHOUR * hours);
+        minutes = time / SECINMINUTE;
+        time = time - (SECINMINUTE * minutes);
+        seconds = time / SECINSECOND;
+        time = time - (SECINSECOND * seconds);
+       
+        System.out.print("The time is: " + hours + ":" + minutes + ":" + seconds);
+    }
+}
