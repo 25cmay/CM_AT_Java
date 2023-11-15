08823e4302e05e71f40c4828dfe11405516d2d45
25cmay <146380979+25cmay@users.noreply.github.com>
2023-11-14 23:35:20 -0500

    Lesson_09
    Lesson_09 Finished

--- Unit_4/Lesson_09/PartD.java
+++ Unit_4/Lesson_09/PartD.java
@@ -2,33 +2,89 @@
 Mr. Eng
 AT Java 
 */
-import java.util.Scanner;
-
-class rollDice {
-    double roll = (Math.random() * 6) + 1;
     
 
-
-
-
-
-
-
-
-
-
-
-
-}
-
-    
-}
-public class PartD {
-    public static void main(String[] args){  
-        for (int i = 1; i <= 10; i++){  
-            int diceRoll = (int) (Math.random() * 6) + 1;
-            System.out.println(diceRoll);
+class DiceRoll{
+    public void updateState(int newState){
+        if (newState == '1'){
+            String[] one = {" _______________",
+                            "|               |",
+                            "|               |",
+                            "|               |",
+                            "|       x       |",
+                            "|               |",
+                            "|               |",
+                            "|_______________|"};
+            System.out.print(one); 
+        } else if (newState == '2'){
+            String[] two = {" _______________",
+                            "|               |",
+                            "|   x           |",
+                            "|               |",
+                            "|               |",
+                            "|               |",
+                            "|            x  |",
+                            "|_______________|"};
+            System.out.print(two);
+        } else if (newState == '3'){
+            String[] three = {  " _______________",
+                                "|               |",
+                                "|  x            |",
+                                "|               |",
+                                "|       x       |",
+                                "|               |",
+                                "|            x  |",
+                                "|_______________|"};
+            System.out.print(three);
+        } else if (newState == '4'){
+            String[] four = {   " _______________",
+                                "|               |",
+                                "|   x       x   |",
+                                "|               |",
+                                "|               |",
+                                "|               |",
+                                "|   x       x   |",
+                                "|_______________|"};
+            System.out.print(four);
+        } else if (newState == '5'){
+            String[] five = {   " _______________",
+                                "|               |",
+                                "|   x       x   |",
+                                "|               |",
+                                "|       x       |",
+                                "|               |",
+                                "|   x       x   |",
+                                "|_______________|"};
+            System.out.print(five);
+        } else if (newState == '6'){
+            String[] six = {    " _______________",
+                                "|               |",
+                                "|   x       x   |",
+                                "|               |",
+                                "|   x       x   |",
+                                "|               |",
+                                "|   x       x   |",
+                                "|_______________|"};
+            System.out.print(six);
+            } 
         }
-            System.out.println();
-   }
+    
+    
+    }
+    
+public class PartD {
+    public static void main (String[] args) throws Exception{
+        DiceRoll roll = new DiceRoll();
+           
+        double number = Math.random();
+        number = number * 6 + 1;
+           
+        System.out.println((int)number);
+        roll.updateState((int)number);
+    
+    
+        }
+        
 }
+
+
