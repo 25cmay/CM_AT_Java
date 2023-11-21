fd10d03f36cf11c31e09d9067ad861a948397ca3
25cmay <146380979+25cmay@users.noreply.github.com>
2023-11-20 22:19:43 -0500

    Lesson_11
    Lesson_11 Completed

---
+++ Unit_5/Lesson_11/PartC.java
@@ -0,0 +1,27 @@
+/* Caleb May
+Mr. Eng
+AT Java 
+*/
+import java.util.Scanner;
+
+public class PartC {
+
+    public static double volumer(double radius) {
+        double volume;
+        volume = Math.pow(radius, 3);
+        volume = volume * 4/3 * Math.PI;
+        return volume;
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner (System.in);
+
+        System.out.print("Enter the radius of your sphere: ");
+        double radius = input.nextDouble();
+
+        double volume = volumer(radius);
+        System.out.println("The volume of your sphere is " + volume);
+
+    }
+}
