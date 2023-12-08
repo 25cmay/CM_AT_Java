88c64bbadc3e7abdf70b33c32770f6a5608a85d8
25cmay <146380979+25cmay@users.noreply.github.com>
2023-12-08 14:15:04 -0500

    AT_08
    AT_08 completed

--- Unit_5/AT_08/PartA.java
+++ Unit_5/AT_08/PartA.java
@@ -5,17 +5,92 @@ class Die {
 
     // consructors
     public Die() {
-       //initialize the do 
-       faceValue = 1;
+        // initialize the do
+        this.roll();
     }
 
-    public Die (int newValue) {
+    public Die(int newValue) {
         faceValue = newValue;
     }
 
     // methods
+    public int roll() {
+        double number = Math.random();
+        faceValue = (int) (number * 6 + 1);
+        return faceValue;
+    }
+
     public int getFaceValue() {
         return faceValue;
+    }
+
+    public void showFace() {
+        String[] face = {};
+        if (faceValue == 1) {
+            String[] one = { " _______________",
+                    "|               |",
+                    "|               |",
+                    "|               |",
+                    "|       x       |",
+                    "|               |",
+                    "|               |",
+                    "|_______________|" };
+            face = one;
+        } else if (faceValue == 2) {
+            String[] two = { " _______________",
+                    "|               |",
+                    "|   x           |",
+                    "|               |",
+                    "|               |",
+                    "|               |",
+                    "|            x  |",
+                    "|_______________|" };
+            face = two;
+        } else if (faceValue == 3) {
+            String[] three = { " _______________",
+                    "|               |",
+                    "|  x            |",
+                    "|               |",
+                    "|       x       |",
+                    "|               |",
+                    "|            x  |",
+                    "|_______________|" };
+            face = three;
+        } else if (faceValue == 4) {
+            String[] four = { " _______________",
+                    "|               |",
+                    "|   x       x   |",
+                    "|               |",
+                    "|               |",
+                    "|               |",
+                    "|   x       x   |",
+                    "|_______________|" };
+            face = four;
+        } else if (faceValue == 5) {
+            String[] five = { " _______________",
+                    "|               |",
+                    "|   x       x   |",
+                    "|               |",
+                    "|       x       |",
+                    "|               |",
+                    "|   x       x   |",
+                    "|_______________|" };
+            face = five;
+        } else if (faceValue == 6) {
+            String[] six = { " _______________",
+                    "|               |",
+                    "|   x       x   |",
+                    "|               |",
+                    "|   x       x   |",
+                    "|               |",
+                    "|   x       x   |",
+                    "|_______________|" };
+            face = six;
+        }
+
+        for (String row:face) {
+            System.out.println(row);
+        }
     }
 
     public void setFaceValue(int newValue) {
@@ -26,24 +101,25 @@ class Die {
         }
     }
 
-
 }
 
-
 public class PartA {
-    public static void main(String[] args ) {
-        Die die = new Die(3);
-
+    public static void main(String[] args) {
+        Die die = new Die();
+        int answer;
         // print current value of the die
-        int answer = die.getFaceValue();
-        System.out.println("The die is equal to " + answer);
+        for (int i = 0; i < 20; i++) {
+            die.roll();
+            answer = die.getFaceValue();
+            System.out.println("The die is equal to " + answer);
+            die.showFace();
+        }
 
         // change value
         die.setFaceValue(6);
         answer = die.getFaceValue();
         System.out.println("The die is equal to " + answer);
+        die.showFace();
     }
-
-
 
 }
