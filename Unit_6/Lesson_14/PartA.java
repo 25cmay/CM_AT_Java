db2759874ce5bc4f951d41e66ff20e18f8f98759
25cmay <146380979+25cmay@users.noreply.github.com>
2023-12-14 09:21:08 -0500

    Lesson_14
    Lesson_14 completed

---
+++ Unit_6/Lesson_14/PartA.java
@@ -0,0 +1,13 @@
+/* Caleb May
+Mr. Eng
+AT Java 
+*/
+/*
+ * 1 = b
+ * 2 = c
+ * 3 = c
+ */
+public class PartA {
+
+    
+}
---
+++ Unit_6/Lesson_14/PartB.java
@@ -0,0 +1,14 @@
+/* Caleb May
+Mr. Eng
+AT Java 
+*/
+
+public class PartB {
+    public static void main(String[] args) throws Exception {
+        double[] numbers = {2.2, 1.0, 3.7, 4.1, 88.0};    
+        for (double number: numbers) {
+        System.out.println(number);
+        }
+    }
+}
+
