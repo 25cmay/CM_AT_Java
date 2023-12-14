906cd4217d469aa037c54f920c82859815197bf0
25cmay <146380979+25cmay@users.noreply.github.com>
2023-12-14 13:31:39 -0500

    Project_5

--- Unit_5/Unit_05_Project/Project1.java
+++ Unit_5/Unit_05_Project/Project1.java
@@ -1,3 +1,73 @@
+/* Caleb May
+Mr. Eng 
+AT Java
+ */
+import java.util.Scanner;
 public class Project1 {
+
+    public static int leadingNumber(String section){
+            int leadingNum = -1;
+            int dotIndex = section.indexOf('.');
+            if (dotIndex == -1) {
+                leadingNum = Integer.parseInt(section);
+                return leadingNum;
+            } else {
+                leadingNum = Integer.parseInt(section.substring(0, dotIndex));
+                return leadingNum;
+            }
+        }
     
+    public static String truncateSection(String section){
+        int dotIndex = section.indexOf('.');
+        if (dotIndex == -1) {
+            return "";
+        }
+        String substring = section.substring(dotIndex + 1);
+        return substring;
+        }
+    private static String compareSections(String section1, String section2) {
+        String truncatedSection1 = truncateSection(section1);
+        String truncatedSection2 = truncateSection(section2);
+
+        if (truncatedSection1.isEmpty() == truncatedSection2.isEmpty()) {
+            return section1;  
+        } else if (truncatedSection1.isEmpty()) {
+            return section1; 
+        } else if (truncatedSection2.isEmpty()) {
+            return section2; 
+        } else {
+            return whoIsFirst(truncatedSection1, truncatedSection2);
+        }
+    }
+
+    public static String whoIsFirst(String section1, String section2){
+        int leadingNumber1 = leadingNumber(section1);
+        int leadingNumber2 = leadingNumber(section2);
+
+        if (leadingNumber1 < leadingNumber2) {
+            return section1;
+        } else if (leadingNumber1 > leadingNumber2) {
+            return section2;
+        } else {
+            return compareSections(section1, section2);
+        }
+    
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first section: ");
+        String section1 = input.nextLine();
+
+        System.out.print("Enter the second section: ");
+        String section2 = input.nextLine();
+
+        String first = whoIsFirst(section1, section2);
+
+        System.out.println("Result: " + first);
+
+    }
+
+
 }
+
--- Unit_5/Unit_05_Project/Project2.java
+++ Unit_5/Unit_05_Project/Project2.java
@@ -1,3 +1,92 @@
+/* Caleb May
+Mr. Eng 
+AT Java
+ */
+import java.util.Scanner;
 public class Project2 {
+
+
+    public static String getEncodedDigit(int d){
+        // Converts numerical digit to barcode value as string
+       String result;
+        if (d == 1) {
+            result = ":::||";
+        } else if (d == 2) {
+            result = "::|:|";
+        } else if (d == 3) {
+            result = "::||:";
+        } else if (d == 4) {
+            result = ":|::|";
+        } else if (d == 5) {
+            result = ":|:|:";
+        } else if (d == 6) {
+            result = ":||::";
+        } else if (d == 7) {
+            result = "|:::|";
+        } else if (d == 8) {
+            result = "|::|:";
+        } else if (d == 9) {
+            result = "|:|::";
+        } else if (d == 0) {
+            result = "||:::";
+        } else {
+            result = "";
+        }
+        return result;
+    } 
+    public static int getCheckDigit(String zipCode){
+        // takes zipcode as string and calculates the sum of the numbers
+        int sum = 0;
+        char c;
+        int val;
+        
+        // go through zip code adding numbers
+        for(int i = 0; i < 5; i++){
+            // get character
+            c = zipCode.charAt(i);
+
+            // convert to int
+            val = Integer.valueOf(c);
+            // Add to sum
+            sum = sum + val;
+
+        }
+        
+        // take modulo of sum
+        int mod = sum % 10;
+        if (mod == 0) {
+            return 0;
+        } else {
+            return 10 - sum;
+        }
+
+
+    }
+    public static String getBarCode(String zipCode) {
+        String result = "|";
+        for (int i = 0; i < zipCode.length(); i++) {
+            int d = Integer.valueOf(zipCode.substring(i, i + 1));
+            result = result + getEncodedDigit(d);
+        }
+        result = result + getEncodedDigit(getCheckDigit(zipCode));
+        result = result + "|";
+
+        return result;
+
+    
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a zipcode: ");
+        String zipcode = input.nextLine();
+        System.out.println(getBarCode(zipcode));
+
+
+    }
+
+
+
+
     
 }
