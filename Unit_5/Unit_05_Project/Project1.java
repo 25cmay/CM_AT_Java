b8fe595e8b1caba6ea6ed2bc413f6c6c5924050f
25cmay <146380979+25cmay@users.noreply.github.com>
2023-12-18 10:54:17 -0500

    Unit_05 Project
    Unit_05 Project fixed

--- Unit_5/Unit_05_Project/Project1.java
+++ Unit_5/Unit_05_Project/Project1.java
@@ -1,57 +1,56 @@
+
 /* Caleb May
 Mr. Eng 
 AT Java
- */
+*/
 import java.util.Scanner;
+
 public class Project1 {
 
-    public static int leadingNumber(String section){
-            int leadingNum = -1;
-            int dotIndex = section.indexOf('.');
-            if (dotIndex == -1) {
-                leadingNum = Integer.parseInt(section);
-                return leadingNum;
-            } else {
-                leadingNum = Integer.parseInt(section.substring(0, dotIndex));
-                return leadingNum;
+    public static String whoIsFirst(String section1, String section2) {
+        String work1 = section1;
+        String work2 = section2;
+
+        while (true) {
+            if (work1.isEmpty()){
+                return section1;
+            } else if (work2.isEmpty()) {
+                return section2;
             }
+            int leadingNumber1 = leadingNumber(work1);
+            int leadingNumber2 = leadingNumber(work2);
+
+            if (leadingNumber1 < leadingNumber2) {
+                return section1;
+            } else if (leadingNumber1 > leadingNumber2) {
+                return section2;
+            }
+
+            work1 = truncateSection(work1);
+            work2 = truncateSection(work2);
         }
-    
-    public static String truncateSection(String section){
+
+    }
+
+    public static int leadingNumber(String section) {
+        int leadingNum = -1;
+        int dotIndex = section.indexOf('.');
+        if (dotIndex == -1) {
+            leadingNum = Integer.parseInt(section);
+            return leadingNum;
+        } else {
+            leadingNum = Integer.parseInt(section.substring(0, dotIndex));
+            return leadingNum;
+        }
+    }
+
+    public static String truncateSection(String section) {
         int dotIndex = section.indexOf('.');
         if (dotIndex == -1) {
             return "";
         }
         String substring = section.substring(dotIndex + 1);
         return substring;
-        }
-    private static String compareSections(String section1, String section2) {
-        String truncatedSection1 = truncateSection(section1);
-        String truncatedSection2 = truncateSection(section2);
-
-        if (truncatedSection1.isEmpty() == truncatedSection2.isEmpty()) {
-            return section1;  
-        } else if (truncatedSection1.isEmpty()) {
-            return section1; 
-        } else if (truncatedSection2.isEmpty()) {
-            return section2; 
-        } else {
-            return whoIsFirst(truncatedSection1, truncatedSection2);
-        }
-    }
-
-    public static String whoIsFirst(String section1, String section2){
-        int leadingNumber1 = leadingNumber(section1);
-        int leadingNumber2 = leadingNumber(section2);
-
-        if (leadingNumber1 < leadingNumber2) {
-            return section1;
-        } else if (leadingNumber1 > leadingNumber2) {
-            return section2;
-        } else {
-            return compareSections(section1, section2);
-        }
-    
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -67,7 +66,4 @@ public class Project1 {
         System.out.println("Result: " + first);
 
     }
-
-
 }
-
