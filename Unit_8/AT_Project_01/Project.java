bc975d9de302dd2f0b19d9c68dda61cdf246550a
Caleb May <cjmay223@gmail.com>
2024-03-16 12:17:05 -0400

    AT_Project_01
    AT_Project_01 broken

--- Unit_8/AT_Project_01/Project.java
+++ Unit_8/AT_Project_01/Project.java
@@ -49,7 +49,7 @@ public class Project {
         // Get row
         System.out.print("Enter a starting row in your yard between 0 and " + (height - 1) + ": ");
         int mrow = in.nextInt();
-        
+
         // Get column
         System.out.print("Enter a starting column in your yard between 0 and " + (width - 1) + ": ");
         int mcolumn = in.nextInt();
@@ -63,37 +63,92 @@ public class Project {
         clearScreen();
         yard.printYard(mower);
 
-        // cut one row.
-        while (true) {
-            int idirection = mdirection;
-            mower.cutGrass(yard);
-            if (mower.checkGrass(yard) == false) {
-                // check right
-                mower.turnRight();
-                if (mower.checkGrass(yard) == true) {
-                    mower.moveForward();
-                } else {
-                    mower.turnLeft();
-                    mower.turnLeft();
-                    if(mower.checkGrass(yard) == true) {
+        // Choose cut
+        System.out.print("Do you want a spiral cut (1) or line by line cut (2): ");
+        int cut = in.nextInt();
+        if (cut == 1) {
+            // cut spiral
+            while (true) {
+                mower.cutGrass(yard);
+                if (mower.checkGrass(yard) == false) {
+                    // check right
+                    mower.turnRight();
+                    if (mower.checkGrass(yard) == true) {
                         mower.moveForward();
                     } else {
-                        break;
+                        mower.turnLeft();
+                        mower.turnLeft();
+                        if (mower.checkGrass(yard) == true) {
+                            mower.moveForward();
+                        } else {
+                            break;
+                        }
+
                     }
-
+                } else {
+                    mower.moveForward();
                 }
-            } else {
-                mower.moveForward();
-            } 
-            clearScreen();
+                clearScreen();
 
-            // Print  yard with mower
-            yard.printYard(mower);
+                // Print yard with mower
+                yard.printYard(mower);
 
-            System.out.println();
-            delay(500);
+                System.out.println();
+                delay(500);
+            }
+        } else if (cut == 2) {
+            // cut line by line
+            int lastdir = mdirection;
+            mower.cutGrass(yard);
+            while (true) {
+                mower.cutGrass(yard);
+                if (mower.checkGrass(yard) == false) {
+                    // check right
+                    mower.turnRight();
+                    if (mower.checkGrass(yard) == true) {
+                        while (mower.checkGrass(yard) == true) {
+                            mower.cutGrass(yard);
+                            mower.moveForward();
+                        }
+                        mower.turnRight();
+                        if (mower.checkGrass(yard) == true) {
+                            mower.moveForward();
+                            mower.cutGrass(yard);
+                            mower.turnRight();
+                        } else {
+                            mower.turnLeft();
+                            mower.turnLeft();
+                            mower.moveForward();
+                            mower.cutGrass(yard);
+                            mower.turnRight();
+                        }
+                    } else {
+                        mower.turnLeft();
+                        mower.turnLeft();
+                        if (mower.checkGrass(yard) == true) {
+                            mower.moveForward();
+                            mower.cutGrass(yard);
+                            mower.turnLeft();
+
+                        } else {
+                            break;
+                        }
+
+                    }
+                } else {
+                    mower.moveForward();
+                    mower.turnLeft();
+                }
+                clearScreen();
+
+                // Print yard with mower
+                yard.printYard(mower);
+
+                System.out.println();
+                delay(500);
+
+            }
+
         }
-
     }
-
 }
