1381c381d07473f2f705139bb86a43db8933f016
Caleb May <cjmay223@gmail.com>
2024-04-18 14:39:18 -0400

    A02
    A02 completed

---
+++ Unit_10/A02/PartB/PartB.java
@@ -0,0 +1,11 @@
+package PartB;
+import PartB.animatePartB.Application;
+
+public class PartB {
+    public static void main(String[] args) {
+        Application myApp = new Application("Demo App");
+        myApp.setVisible(true);
+        
+    }
+    
+}
